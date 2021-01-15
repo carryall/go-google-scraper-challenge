478c9d6b1274967e3cc98da7132d20926b323c8a
Carryall <carryall369@gmail.com>
2021-01-15 17:35:06 +0700

    move database setup to initializer

---
+++ initializers/database.go
@@ -0,0 +1,32 @@
+package initializers
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/beego/beego/v2/client/orm"
+	"github.com/beego/beego/v2/server/web"
+	_ "github.com/lib/pq"
+)
+
+func SetUpDatabase() {
+	dbURL, err := web.AppConfig.String("db_url")
+	if err != nil {
+		log.Fatal("Database URL not found: ", err)
+	}
+
+	err = orm.RegisterDriver("postgres", orm.DRPostgres)
+	if err != nil {
+		fmt.Println("Postgres Driver registration failed: ", err)
+	}
+
+	err = orm.RegisterDataBase("default", "postgres", dbURL)
+	if err != nil {
+		fmt.Println("Database Registration failed: ", err)
+	}
+
+	err = orm.RunSyncdb("default", false, true)
+	if err != nil {
+		fmt.Println("Database Sync failed: ", err)
+	}
+}
