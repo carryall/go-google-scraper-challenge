1341389e9fd71c58424c61bfd615f8a7c570764e
Carryall <carryall369@gmail.com>
2021-01-18 21:10:52 +0700

    remove unneeded env load from main

--- main.go
+++ main.go
@@ -3,19 +3,12 @@ package main
 import (
 	"go-google-scraper-challenge/initializers"
 	_ "go-google-scraper-challenge/routers"
-	"log"
 
 	"github.com/beego/beego/v2/server/web"
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	initializers.SetUpDatabase()
 }
 
