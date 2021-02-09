c9645b863b453bb6e9d14dcd1c8f4ef8d0489fbf
Carryall <carryall369@gmail.com>
2021-02-09 17:32:48 +0700

    add missing test on form

--- forms/session_test.go
+++ forms/session_test.go
@@ -5,11 +5,81 @@ import (
 	"go-google-scraper-challenge/initializers"
 	. "go-google-scraper-challenge/test/helpers"
 
+	"github.com/beego/beego/v2/core/validation"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Forms/SessionForm", func() {
+	Describe("#Valid", func() {
+		Context("given session form with valid params", func() {
+			It("does NOT add error to validation", func() {
+				FabricateUser("dev@nimblehq.co", "password")
+				form := forms.SessionForm{
+					Email:    "dev@nimblehq.co",
+					Password: "password",
+				}
+
+				formValidation := validation.Validation{}
+				form.Valid(&formValidation)
+
+				Expect(len(formValidation.Errors)).To(BeZero())
+			})
+		})
+
+		Context("given session form with INVALID params", func() {
+			Context("given user email does NOT exist", func() {
+				It("adds an error to validation", func() {
+					form := forms.SessionForm{
+						Email:    "john.doe@nimblehq.co",
+						Password: "password",
+					}
+
+					formValidation := validation.Validation{}
+					form.Valid(&formValidation)
+
+					Expect(len(formValidation.Errors)).To(Equal(1))
+					Expect(formValidation.Errors[0].Key).To(Equal("Email"))
+					Expect(formValidation.Errors[0].Message).To(Equal("Incorrect email or password"))
+				})
+			})
+
+			Context("given email is INVALID", func() {
+				It("adds an error to validation", func() {
+					FabricateUser("dev@nimblehq.co", "password")
+					form := forms.SessionForm{
+						Email:    "wrong.email@nimblehq.co",
+						Password: "password",
+					}
+
+					formValidation := validation.Validation{}
+					form.Valid(&formValidation)
+
+					Expect(len(formValidation.Errors)).To(Equal(1))
+					Expect(formValidation.Errors[0].Key).To(Equal("Email"))
+					Expect(formValidation.Errors[0].Message).To(Equal("Incorrect email or password"))
+				})
+			})
+
+			Context("given password is INVALID", func() {
+				It("adds an error to validation", func() {
+					FabricateUser("dev@nimblehq.co", "password")
+					form := forms.SessionForm{
+						Email:    "dev@nimblehq.co",
+						Password: "wrong password",
+					}
+
+					formValidation := validation.Validation{}
+					form.Valid(&formValidation)
+
+					Expect(len(formValidation.Errors)).To(Equal(1))
+					Expect(formValidation.Errors[0].Key).To(Equal("Password"))
+					Expect(formValidation.Errors[0].Message).To(Equal("Incorrect email or password"))
+				})
+			})
+		})
+	})
+
 	Describe("#Save", func() {
 		Context("given session form with valid params", func() {
 			It("returns user with NO error", func() {
@@ -27,8 +97,8 @@ var _ = Describe("Forms/SessionForm", func() {
 		})
 
 		Context("given session form with INVALID params", func() {
-			Context("given email is not an valid email", func() {
-				It("returns a email is invalid error", func() {
+			Context("given email is NOT valid", func() {
+				It("returns an email is invalid error", func() {
 					form := forms.SessionForm{
 						Email:    "not an email",
 						Password: "password",
