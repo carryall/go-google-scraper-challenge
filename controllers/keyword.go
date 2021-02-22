bc8aaf5c2175f6877673151fe885994d8ae8e0c4
Carryall <carryall369@gmail.com>
2021-02-22 18:59:05 +0700

    try async

--- controllers/keyword.go
+++ controllers/keyword.go
@@ -23,9 +23,17 @@ func (c *KeywordController) List() {
 
 	web.ReadFromRequest(&c.Controller)
 
-	var keyword string
-	c.Ctx.Input.Bind(&keyword, "keyword")
-	if len(keyword) > 0 {
-		helpers.Scrape(keyword)
+	//var keyword string
+	//c.Ctx.Input.Bind(&keyword, "keyword")
+	//if len(keyword) > 0 {
+	//	helpers.Scrape(keyword)
+	//}
+
+	keywords := []string{
+		"ergonomic chair",
+		"cloud storage service",
+		"cloud computing service",
+		"crypto currency",
 	}
+	helpers.Scrape(keywords)
 }
--- helpers/scraper.go
+++ helpers/scraper.go
@@ -1,24 +1,48 @@
 package helpers
 
 import (
+	"fmt"
 	"log"
 	"net/url"
 
 	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/debug"
 	"github.com/gocolly/colly/extensions"
+	"github.com/gocolly/colly/queue"
 )
 
 var selectors = map[string]string {
 	"topImageAds": "#tvcap .plantl a.pla-unit-title-link",
 	"topLinkAds": "#tvcap .d5oMvf > a",
-	"sideImageAds": ".commercial-unit-desktop-rhs a.pla-unit-single-clickable-target",
+	"sideImageAds": ".cu-container a.plantl.clickable-card",
 	"nonAds": "#search .yuRUbf > a",
 	"bottomAds": "#bottomads .uEierd > a",
 }
 
-func Scrape(keyword string) {
-	keyword = url.QueryEscape(keyword)
-	collector := colly.NewCollector()
+const GOOGLE_SEARCH_URL = "http://www.google.com/search?q=%s"
+
+func Scrape(keywords []string) {
+	collector := colly.NewCollector(
+			colly.Debugger(&debug.LogDebugger{}),
+			colly.CacheDir("./cache/"),
+			colly.Async(true),
+		)
+
+	q, _ := queue.New(
+		2, // Number of consumer threads
+		&queue.InMemoryQueueStorage{MaxSize: 10000}, // Use default queue storage
+	)
+
+	collector.Limit(&colly.LimitRule{
+		DomainGlob:  "*httpbin.*",
+		Parallelism: 2,
+		//Delay:      5 * time.Second,
+	})
+
+	for _, keyword := range keywords {
+		excapedKeyword := url.QueryEscape(keyword)
+		q.AddURL(fmt.Sprintf(GOOGLE_SEARCH_URL, excapedKeyword))
+	}
 
 	extensions.RandomUserAgent(collector)
 
@@ -75,8 +99,13 @@ func Scrape(keyword string) {
 		}
 	})
 
-	err := collector.Visit("http://www.google.com/search?q="+keyword)
-	if err != nil {
-		log.Fatal("Fail to start scraping ", err.Error())
-	}
+	collector.OnError(func(r *colly.Response, err error) {
+		log.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	q.Run(collector)
+	//err := collector.Visit(GOOGLE_SEARCH_URL + keyword)
+	//if err != nil {
+	//	log.Fatal("Fail to start scraping ", err.Error())
+	//}
 }
