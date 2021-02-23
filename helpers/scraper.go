ee972910d8aabb8941e6146e1a603a29352a59ad
Carryall <carryall369@gmail.com>
2021-02-23 22:21:00 +0700

    finish scraper

--- helpers/scraper.go
+++ helpers/scraper.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/url"
 
+	"github.com/beego/beego/v2/server/web"
 	"github.com/gocolly/colly"
 	"github.com/gocolly/colly/debug"
 	"github.com/gocolly/colly/extensions"
@@ -19,24 +20,39 @@ var selectors = map[string]string {
 	"bottomAds": "#bottomads .uEierd > a",
 }
 
+type SearchResult struct {
+	Keyword string
+	TopAdLinks []string
+	OtherAdLinks []string
+	NonAdLinks []string
+	PageCache string
+}
+
 const GOOGLE_SEARCH_URL = "http://www.google.com/search?q=%s"
+const currentBrowser = "Chrome/88.0.4324.182"
+const currentOs = "Macintosh; Intel Mac OS X 10_15_5"
 
-func Scrape(keywords []string) {
-	collector := colly.NewCollector(
-			colly.Debugger(&debug.LogDebugger{}),
-			colly.CacheDir("./cache/"),
-			colly.Async(true),
-		)
+func Search(keywords []string) {
+	runMode, err := web.AppConfig.String("runmode")
+	if err != nil {
+		log.Fatal("Run mode not found: ", err)
+	}
 
-	q, _ := queue.New(
-		2, // Number of consumer threads
-		&queue.InMemoryQueueStorage{MaxSize: 10000}, // Use default queue storage
-	)
+	results := map[string] *SearchResult{}
+	collector := colly.NewCollector(colly.Async(true))
+
+	if runMode == "dev" {
+		collector.SetDebugger(&debug.LogDebugger{})
+	}
+
+	q, err := queue.New(2, &queue.InMemoryQueueStorage{MaxSize: 10000})
+	if err != nil {
+		log.Fatal("Failed to create a queue", err.Error())
+	}
 
 	collector.Limit(&colly.LimitRule{
 		DomainGlob:  "*httpbin.*",
 		Parallelism: 2,
-		//Delay:      5 * time.Second,
 	})
 
 	for _, keyword := range keywords {
@@ -46,66 +62,107 @@ func Scrape(keywords []string) {
 
 	extensions.RandomUserAgent(collector)
 
-	collector.OnRequest(func(r *colly.Request) {
-		log.Println("Visiting", r.URL)
-	})
-
-	collector.OnResponse(func(r *colly.Response) {
-		log.Println("Visited", r.Request.URL)
-	})
+	collector.OnRequest(RequestHandler)
+	collector.OnResponse(ResponseHandler)
+	collector.OnError(ErrorHandler)
 
 	collector.OnHTML(selectors["nonAds"], func(e *colly.HTMLElement) {
-		log.Println("NON ADs ===============")
-		href := e.Attr("href")
-		if len(href) > 0 {
-			log.Println("	Title", e.Text)
-			log.Println("	Link", e.Attr("href"))
+		link := e.Attr("href")
+		if len(link) > 0 {
+			keyword := e.Request.Ctx.Get("keyword")
+			_, isExist := results[keyword]
+			if !isExist {
+				results[keyword] = &SearchResult{Keyword: keyword}
+			}
+			results[keyword].NonAdLinks = append(results[keyword].NonAdLinks, link)
 		}
 	})
 
 	collector.OnHTML(selectors["topImageAds"], func(e *colly.HTMLElement) {
-		log.Println("TOP IMAGE ADs ===============")
-		href := e.Attr("href")
-		if len(href) > 0 {
-			log.Println("	Title", e.Text)
-			log.Println("	Link", e.Attr("href"))
+		keyword := e.Request.Ctx.Get("keyword")
+		link := e.Attr("href")
+		if len(link) > 0 {
+
+			_, isExist := results[keyword]
+			if !isExist {
+				results[keyword] = &SearchResult{Keyword: keyword}
+			}
+			results[keyword].TopAdLinks = append(results[keyword].TopAdLinks, link)
 		}
 	})
 
 	collector.OnHTML(selectors["topLinkAds"], func(e *colly.HTMLElement) {
-		log.Println("TOP LINK ADs ===============")
-		href := e.Attr("href")
-		if len(href) > 0 {
-			log.Println("	Title", e.Text)
-			log.Println("	Link", e.Attr("href"))
+		keyword := e.Request.Ctx.Get("keyword")
+		link := e.Attr("href")
+		if len(link) > 0 {
+			_, isExist := results[keyword]
+			if !isExist {
+				results[keyword] = &SearchResult{Keyword: keyword}
+			}
+			results[keyword].TopAdLinks = append(results[keyword].TopAdLinks, link)
 		}
 	})
 
 	collector.OnHTML(selectors["sideImageAds"], func(e *colly.HTMLElement) {
-		log.Println("SIDE IMAGE ADs ===============")
-		href := e.Attr("href")
-		if len(href) > 0 {
-			log.Println("	Title", e.Text)
-			log.Println("	Link", e.Attr("href"))
+		link := e.Attr("href")
+		if len(link) > 0 {
+			keyword := e.Request.Ctx.Get("keyword")
+			_, isExist := results[keyword]
+			if !isExist {
+				results[keyword] = &SearchResult{Keyword: keyword}
+			}
+			results[keyword].OtherAdLinks = append(results[keyword].OtherAdLinks, link)
 		}
 	})
 
 	collector.OnHTML(selectors["bottomAds"], func(e *colly.HTMLElement) {
-		log.Println("BOTTOM ADs ===============")
-		href := e.Attr("href")
-		if len(href) > 0 {
-			log.Println("	Title", e.Text)
-			log.Println("	Link", e.Attr("href"))
+		link := e.Attr("href")
+		if len(link) > 0 {
+			keyword := e.Request.Ctx.Get("keyword")
+			_, isExist := results[keyword]
+			if !isExist {
+				results[keyword] = &SearchResult{Keyword: keyword}
+			}
+			results[keyword].OtherAdLinks = append(results[keyword].OtherAdLinks, link)
 		}
 	})
 
-	collector.OnError(func(r *colly.Response, err error) {
-		log.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	collector.OnScraped(func(r *colly.Response) {
+		if q.IsEmpty() {
+			log.Println("Finished scraping ==========")
+			for _, result := range results {
+				log.Println("Keyword:", result.Keyword)
+				log.Println("	Top Ad:", len(result.TopAdLinks))
+				log.Println("	Non Ad:", len(result.NonAdLinks))
+				log.Println("	Other Ad:", len(result.OtherAdLinks))
+			}
+		}
 	})
 
 	q.Run(collector)
-	//err := collector.Visit(GOOGLE_SEARCH_URL + keyword)
-	//if err != nil {
-	//	log.Fatal("Fail to start scraping ", err.Error())
-	//}
+}
+
+func RequestHandler (r *colly.Request) {
+	userAgent := fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/537.36 (KHTML, like Gecko) %s Safari/537.36", currentOs, currentBrowser)
+	r.Headers.Set("User-Agent", userAgent)
+
+	log.Println("Visiting", r.URL)
+	r.Ctx.Put("keyword", keywordFromUrl(r.URL.String()))
+}
+
+func ResponseHandler (r *colly.Response) {
+	log.Println("Visited", r.Request.URL)
+}
+
+func ErrorHandler(r *colly.Response, err error) {
+	log.Println("Failed to request URL:", r.Request.URL, "with response:", r, "\nError:", err)
+}
+
+func keywordFromUrl(urlStr string) (keyword string) {
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		log.Println("Failed to parse url string", err.Error())
+	}
+
+	return u.Query().Get("q")
 }
--- routers/router.go
+++ routers/router.go
@@ -8,16 +8,21 @@ import (
 )
 
 func init() {
-	web.Router("/", &controllers.KeywordController{}, "get:List")
+	//
+	web.Router("/", &controllers.SearchResultController{}, "get:List")
+	web.Router("/search_results", &controllers.SearchResultController{}, "post:Create")
+
+	// Authentication
 	web.Router("/signup", &controllers.UserController{}, "get:New")
 	web.Router("/users", &controllers.UserController{}, "post:Create")
-
 	web.Router("/signin", &controllers.SessionController{}, "get:New")
 	web.Router("/signout", &controllers.SessionController{}, "get:Delete")
 	web.Router("/sessions", &controllers.SessionController{}, "post:Create")
 
+	// OAuth Client
 	web.Router("/oauth_client", &controllers.OAuthClientController{}, "get:New;post:Create")
 	web.Router("/oauth_client/:id", &controllers.OAuthClientController{}, "get:Show")
 
+	// API
 	web.Router("/api/v1/login", &apicontrollers.AuthController{}, "post:Login")
 }
