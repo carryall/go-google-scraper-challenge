1c9faf9cef65ada48a9d96e179aea72c541a80b8
Carryall <carryall369@gmail.com>
2022-05-31 16:49:01 +0700

    Response with the serializer

--- lib/api/v1/controllers/authentication.go
+++ lib/api/v1/controllers/authentication.go
@@ -6,7 +6,9 @@ import (
 
 	"go-google-scraper-challenge/constants"
 	. "go-google-scraper-challenge/helpers/api"
+	helpers "go-google-scraper-challenge/helpers/api"
 	"go-google-scraper-challenge/lib/api/v1/forms"
+	"go-google-scraper-challenge/lib/api/v1/serializers"
 	"go-google-scraper-challenge/lib/services/oauth"
 
 	"github.com/gin-gonic/gin"
@@ -38,9 +40,18 @@ func (c *AuthenticationController) Login(ctx *gin.Context) {
 		return
 	}
 
-	err = oauth.HandleTokenRequest(ctx)
+	tokenData, err := oauth.HandleTokenRequest(ctx)
 	if err != nil {
 		ResponseWithError(ctx, http.StatusUnauthorized, errors.New(constants.OAuthClientInvalid), constants.ERROR_CODE_INVALID_CREDENTIALS)
 		return
 	}
+
+	response := &serializers.AuthenticationResponse{
+		AccessToken:  tokenData["access_token"].(string),
+		RefreshToken: tokenData["refresh_token"].(string),
+		ExpiresIn:    tokenData["expires_in"].(int64),
+		TokenType:    tokenData["token_type"].(string),
+	}
+
+	helpers.RenderJSON(ctx, http.StatusOK, response)
 }
