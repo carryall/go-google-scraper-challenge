25fac3157577ef173038d3f74770ab3ca99c2551
Carryall <carryall369@gmail.com>
2022-05-31 17:16:12 +0700

    Update the oauth clisnt api

--- lib/api/v1/controllers/oauth/clients.go
+++ lib/api/v1/controllers/oauth/clients.go
@@ -4,7 +4,9 @@ import (
 	"net/http"
 
 	. "go-google-scraper-challenge/helpers/api"
+	helpers "go-google-scraper-challenge/helpers/api"
 	"go-google-scraper-challenge/lib/api/v1/controllers"
+	"go-google-scraper-challenge/lib/api/v1/serializers"
 	"go-google-scraper-challenge/lib/services/oauth"
 
 	"github.com/gin-gonic/gin"
@@ -21,5 +23,10 @@ func (c *OAuthClientsController) Create(ctx *gin.Context) {
 		ResponseWithError(ctx, http.StatusUnprocessableEntity, err, "")
 	}
 
-	ctx.JSON(http.StatusOK, oauthClient)
+	response := &serializers.OAuthClientResponse{
+		ClientID:     oauthClient.ClientID,
+		ClientSecret: oauthClient.ClientSecret,
+	}
+
+	helpers.RenderJSON(ctx, http.StatusOK, response)
 }
