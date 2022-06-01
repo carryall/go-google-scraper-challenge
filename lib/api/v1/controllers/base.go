026c02167ee492abc1f9697e52c5b61ca1984bda
Carryall <carryall369@gmail.com>
2022-06-01 14:33:54 +0700

    Add route and controller

--- lib/api/v1/controllers/base.go
+++ lib/api/v1/controllers/base.go
@@ -1,3 +1,41 @@
 package controllers
 
-type BaseController struct{}
+import (
+	"go-google-scraper-challenge/constants"
+	. "go-google-scraper-challenge/helpers/api"
+	"go-google-scraper-challenge/lib/models"
+	"go-google-scraper-challenge/lib/services/oauth"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type BaseController struct {
+	CurrentUser *models.User
+}
+
+func (b *BaseController) EnsureAuthenticatedUser(ctx *gin.Context) {
+	currentUser, err := b.GetCurrentUser(ctx)
+	if err != nil {
+		ResponseWithError(ctx, http.StatusUnauthorized, err, constants.TokenInvalid)
+	}
+
+	b.CurrentUser = currentUser
+}
+
+func (b *BaseController) GetCurrentUser(ctx *gin.Context) (user *models.User, err error) {
+	tokenInfo, err := oauth.ValidateToken(ctx.Request)
+	if err != nil {
+		return
+	}
+
+	userID, err := strconv.Atoi(tokenInfo.GetUserID())
+	if err != nil {
+		return
+	}
+
+	user, err = models.GetUserByID(int64(userID))
+
+	return
+}
