1e228afed2904496fc317461114e08e64b7dd7ce
Carryall <carryall369@gmail.com>
2022-06-15 11:27:35 +0700

    [#63] Adjust the result list response to have fewer attr

--- lib/api/v1/serializers/result.go
+++ lib/api/v1/serializers/result.go
@@ -2,9 +2,22 @@ package serializers
 
 import (
 	"go-google-scraper-challenge/lib/models"
+	"time"
 )
 
 type ResultResponse struct {
+	ID          int64         `jsonapi:"primary,result"`
+	Keyword     string        `jsonapi:"attr,keyword"`
+	UserID      int64         `jsonapi:"attr,user_id"`
+	Status      string        `jsonapi:"attr,status"`
+	AdLinkCount int           `jsonapi:"attr,ad_link_count"`
+	LinkCount   int           `jsonapi:"attr,link_count"`
+	User        *UserResponse `jsonapi:"relation,user,omitempty"`
+	CreatedAt   time.Time     `jsonapi:"attr,created_at"`
+	UpdatedAt   time.Time     `jsonapi:"attr,updated_at"`
+}
+
+type ResultDetailResponse struct {
 	ID        int64             `jsonapi:"primary,result"`
 	Keyword   string            `jsonapi:"attr,keyword"`
 	UserID    int64             `jsonapi:"attr,user_id"`
@@ -17,6 +30,31 @@ type ResultResponse struct {
 
 type ResultsJSONResponse struct {
 	Data []struct {
+		ID         string `json:"id"`
+		Attributes struct {
+			Keyword     string `json:"keyword"`
+			UserID      int64  `json:"user_id"`
+			Status      string `json:"status"`
+			AdLinkCount int    `json:"ad_link_count"`
+			LinkCount   int    `json:"link_count"`
+			CreatedAt   int    `json:"created_at"`
+			UpdatedAt   int    `json:"updated_at"`
+		} `json:"attributes"`
+		Relationships struct {
+			User struct {
+				Data RelationshipData `json:"data"`
+			} `json:"user"`
+		}
+	} `json:"data"`
+	Included []struct {
+		ID         string                 `json:"id"`
+		Type       string                 `json:"type"`
+		Attributes map[string]interface{} `json:"attributes"`
+	} `json:"included"`
+}
+
+type ResultDetailJSONResponse struct {
+	Data struct {
 		ID         string `json:"id"`
 		Attributes struct {
 			Keyword   string `json:"keyword"`
@@ -49,6 +87,35 @@ type ResultSerializer struct {
 
 func (s ResultSerializer) Response() (response *ResultResponse) {
 	response = &ResultResponse{
+		ID:        s.Result.ID,
+		Keyword:   s.Result.Keyword,
+		UserID:    s.Result.UserID,
+		Status:    s.Result.Status,
+		CreatedAt: s.Result.CreatedAt,
+		UpdatedAt: s.Result.UpdatedAt,
+	}
+
+	if s.Result.User != nil {
+		response.User = UserSerializer{User: s.Result.User}.Response()
+	}
+
+	if s.Result.AdLinks != nil {
+		response.AdLinkCount = len(s.Result.AdLinks)
+	} else {
+		response.AdLinkCount = 0
+	}
+
+	if s.Result.Links != nil {
+		response.LinkCount = len(s.Result.Links)
+	} else {
+		response.LinkCount = 0
+	}
+
+	return response
+}
+
+func (s ResultSerializer) DetailResponse() (response *ResultDetailResponse) {
+	response = &ResultDetailResponse{
 		ID:        s.Result.ID,
 		Keyword:   s.Result.Keyword,
 		UserID:    s.Result.UserID,
