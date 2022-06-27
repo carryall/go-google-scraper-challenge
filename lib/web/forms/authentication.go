a118ebcea8e963bd37c091bca12b07a553fbd41b
Carryall <carryall369@gmail.com>
2022-06-27 17:50:56 +0700

    [#118] Add form

---
+++ lib/web/forms/authentication.go
@@ -0,0 +1,37 @@
+package webforms
+
+import (
+	"errors"
+	"go-google-scraper-challenge/constants"
+	"go-google-scraper-challenge/lib/models"
+
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+)
+
+type AuthenticationForm struct {
+	Email    string `form:"email"`
+	Password string `form:"password"`
+}
+
+func (f AuthenticationForm) Validate() (valid bool, err error) {
+	err = validation.ValidateStruct(&f,
+		validation.Field(&f.Email, validation.Required, is.Email),
+		validation.Field(&f.Password, validation.Required),
+	)
+
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func (f AuthenticationForm) ValidateUser() error {
+	_, err := models.GetUserByEmail(f.Email)
+	if err != nil {
+		return errors.New(constants.UserDoesNotExist)
+	}
+
+	return nil
+}
