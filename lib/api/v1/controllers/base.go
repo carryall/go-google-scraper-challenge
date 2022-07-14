87bb8a0e5fc2ed44534cee8403cd7d37ff081d09
Carryall <carryall369@gmail.com>
2022-07-14 18:26:57 +0700

    [#96] Add a function to get current user

--- lib/api/v1/controllers/base.go
+++ lib/api/v1/controllers/base.go
@@ -1,3 +1,19 @@
 package controllers
 
+import (
+	"go-google-scraper-challenge/constants"
+	"go-google-scraper-challenge/lib/models"
+
+	"github.com/gin-gonic/gin"
+)
+
 type BaseController struct{}
+
+func (c *BaseController) GetCurrentUser(ctx *gin.Context) *models.User {
+	currentUser := ctx.MustGet(constants.ContextCurrentUser)
+	if currentUser == nil {
+		return nil
+	}
+
+	return currentUser.(*models.User)
+}
