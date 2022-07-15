18361831cf165b67a95da716098f23a219177a77
Carryall <carryall369@gmail.com>
2022-07-15 13:11:03 +0700

    [#119] Add view helepr to format date time

---
+++ helpers/date.go
@@ -0,0 +1,11 @@
+package helpers
+
+import (
+	"time"
+)
+
+const DateTimeLayout = "02/01/2006 15:04"
+
+func FormatDateTime(dateTime time.Time) string {
+	return dateTime.Local().Format(DateTimeLayout)
+}
