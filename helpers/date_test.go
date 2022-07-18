afbd0266d2e8340c8132920bf26d1a56594e75d8
Carryall <carryall369@gmail.com>
2022-07-18 10:40:14 +0700

    [#119] Set the timezone

--- helpers/date_test.go
+++ helpers/date_test.go
@@ -13,7 +13,7 @@ var _ = Describe("Date", func() {
 	Describe("#FormatDateTime", func() {
 		Context("given a date time", func() {
 			It("returns a formatted date time in string", func() {
-				dateTime := time.Date(2022, time.Month(5), 20, 7, 30, 35, 0, time.FixedZone("UTC+7", 7*60*60))
+				dateTime := time.Date(2022, time.Month(5), 20, 7, 30, 35, 0, time.UTC)
 				htmlDate := helpers.FormatDateTime(dateTime)
 
 				Expect(htmlDate).To(Equal("20/05/2022 14:30"))
