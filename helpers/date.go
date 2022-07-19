498d93cd70f3bb128a9ee921df86ac6cef1dae84
Carryall <carryall369@gmail.com>
2022-07-19 18:27:18 +0700

    [#119] Use parentheses on const

--- helpers/date.go
+++ helpers/date.go
@@ -4,7 +4,9 @@ import (
 	"time"
 )
 
-const DateTimeLayout = "02/01/2006 15:04"
+const (
+	DateTimeLayout = "02/01/2006 15:04"
+)
 
 func FormatDateTime(dateTime time.Time) string {
 	return dateTime.Local().Format(DateTimeLayout)
